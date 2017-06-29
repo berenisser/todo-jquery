9a20d0e4b92d6d737f11783ceca2d35d595d72a7
berenisser <berenisser@gmail.com>
2017-06-29 10:48:25 -0400

    local storage listo

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,7 @@
 $(document).ready(function(){
 	var counter = 0;
+	$('#items').html(localStorage.getItem('listItems'));
+	$('#completed').html(localStorage.getItem('listItems2'));
 
 	$("#tarea-input").keypress(function(e){
 		var key = e.which;
@@ -11,45 +13,64 @@ $(document).ready(function(){
 				alert("You should add a task");
 			} else{
 				var crearTask = function(taskToAdd) {
-				  var nuevoDiv = $('<div class="tarea-padre"></div>');
-				  var nuevoCheckbox = $('<input type="checkbox" id="idtask-'+counter +' " class="checkbox">');
-				  var nuevoLabel = $('<label for="idtask-'+counter +' "></label>');
-				  var removeButton = $('<a class="waves-effect waves-light btn right remove">Remove</a>');
+					var nuevoDiv = $('<div class="tarea-padre"></div>');
+					var nuevoCheckbox = $('<input type="checkbox" id="idtask-'+counter +' " class="checkbox">');
+					var nuevoLabel = $('<label for="idtask-'+counter +' "></label>');
+					var removeButton = $('<a class="waves-effect waves-light btn right remove">Remove</a>');
 
-				  nuevoDiv.append(nuevoCheckbox)
-				  .append(nuevoLabel.html(taskToAdd))
-				  .append(removeButton);
+					nuevoDiv.append(nuevoCheckbox)
+					.append(nuevoLabel.html(taskToAdd))
+					.append(removeButton);
 
-				  return nuevoDiv;
+					return nuevoDiv;
 				} 
-
 
 				var listItemToAdd = crearTask(tareaInput);
 				$("#items").append(listItemToAdd);
 				//$("#items").append('<div class="tarea-padre"><input type="checkbox" class="filled-in" id="idtask"/><label for="idtask">'+ tareaInput +'</label><a class="waves-effect waves-light btn right">Remove</a></div>');
 				//return false;
+				localStorage.setItem('listItems', $('#items').html());
 
 				counter++;
 			}
-
-			//Para remover la tarea de clase .remove
-			$('.remove').click(function(){
-				$(this).parent().remove();
-			})
-
-			//cuando se le da click al checkbox en la seccion "To do" 
-			$("#items").on( "click", "input.checkbox", function(){ 
-			 var listItem = $(this).parent();
-			 $("#completed").append(listItem);
-			})
-
-			//cuando se le da click al checkbox en la seccion "Completed" 
-			$("#completed").on( "click", "input.checkbox", function(){
-			 var listItem = $(this).parent();
-			 $("#items").append(listItem);
-			})
 		}
-	})
+
+		//llamar funciones
+		remove();
+		check();
+	});
+
+	//Para remover la tarea de clase .remove me aseguro que sean de ambas "listas"
+	function remove(){
+		$('.remove').click(function(){
+			$(this).parent().remove();
+			localStorage.removeItem('listItems');
+			localStorage.removeItem('listItems2');
+		})
+	}
+		
+	function check(){
+		//cuando se le da click al checkbox en la seccion "To do" debe ir a la seccion de completado
+		$("#items").on( "click", "input.checkbox", function(){ 
+			var listItem = $(this).parent();
+			$("#completed").append(listItem);
+			localStorage.setItem('listItems2', $('#completed').html());
+			localStorage.setItem('listItems', $('#items').html());
+		})
+	}
+		
+
+	/*
+	//cuando se le da click al checkbox en la seccion "Completed" 
+	$("#completed").on( "click", "input.checkbox", function(){
+	 var listItem = $(this).parent();
+	 $("#items").append(listItem);
+	 //localStorage.setItem('listItems', $('#items').html());
+	})*/
+
+	//llamar funciones
+	remove();
+	check();
 });
 
 
